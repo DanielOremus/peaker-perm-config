1d6675586135cf45cb0ee69ccce71223ba8405d5
Daniel <oremusd89@gmail.com>
2025-06-20 14:14:02 +0300

    updated to v1.0.2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const config = Object.freeze({
   products: ["create", "read", "update", "delete"],
   roles: ["create", "read", "update", "delete"],
+  users: ["create", "read", "update", "delete"],
 })
 
 export { config }
