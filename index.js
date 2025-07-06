d20cad35ba09759bf05b38128c915d56a88e4863
Daniel <oremusd89@gmail.com>
2025-07-06 19:56:17 +0300

    updated to v2.0.0

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-const config = Object.freeze({
+export const getServerPermissionsConfig = (schemaType) => ({
+  products: {
+    type: schemaType,
+    default: {
+      create: false,
+      read: true,
+      update: false,
+      delete: false,
+    },
+  },
+  roles: {
+    type: schemaType,
+    default: {
+      create: false,
+      read: false,
+      update: false,
+      delete: false,
+    },
+  },
+  users: {
+    type: schemaType,
+    default: {
+      create: false,
+      read: false,
+      update: false,
+      delete: false,
+    },
+  },
+})
+
+export const clientPermissionsConfig = {
   products: ["create", "read", "update", "delete"],
   roles: ["create", "read", "update", "delete"],
   users: ["create", "read", "update", "delete"],
-})
-
-export { config }
+}
